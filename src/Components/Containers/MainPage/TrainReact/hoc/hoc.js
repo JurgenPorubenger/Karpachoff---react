3d90dae247f31438b136253838460a6cbc07ba01
Eugene <jurgen.porubenger@gmail.com>
2020-01-24 16:47:34 +0200

    add HOOKS2

--- src/Components/Containers/MainPage/TrainReact/hoc/hoc.js
+++ src/Components/Containers/MainPage/TrainReact/hoc/hoc.js
@@ -1,41 +1,16 @@
-import React from 'react';
-import axiosHTTP from '../Fetch/fetch';
+import React, { useState, useEffect } from 'react';
+import fetchInfo from '../Fetch/fetch';
 
 // Эта функция принимает компонент...
 // eslint-disable-next-line no-unused-vars
 export default function withSubscription(WrappedComponent, selectData) {
   // ...и возвращает другой компонент...
-  return class extends React.Component {
-    constructor(props) {
-      super(props);
-      this.handleChange = this.handleChange.bind(this);
-      this.state = {
-        data: [],
-      };
-    }
-
-    componentDidMount() {
-      this.setState({
-        data: selectData(axiosHTTP, this.props.url),
-      });
-      console.log(selectData(axiosHTTP, this.props.url));
-
-      // ...который подписывается на оповещения...
-      // DataSource.addChangeListener(this.handleChange);
-    }
-
-    componentWillUnmount() {
-      // DataSource.removeChangeListener(this.handleChange);
-    }
-
-    handleChange() {
-
-    }
-
-    render() {
-      // ... и рендерит оборачиваемый компонент со свежи  ми данными!
-      // Обратите внимание, что мы передаём остальные пропсы
-      return <WrappedComponent data={this.state.data} />;
-    }
+  return (props) => {
+    const [data, setData] = useState([]);
+    // eslint-disable-next-line react/prop-types,react/destructuring-assignment
+    useEffect(async () => { setData(await selectData(fetchInfo, props.url)); }, props.url);
+    // ... и рендерит оборачиваемый компонент со свежими данными!
+    // Обратите внимание, что мы передаём остальные пропсы
+    return <WrappedComponent data={data} {...props} />;
   };
 }
